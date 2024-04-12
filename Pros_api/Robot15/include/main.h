ce2503df7026a049920f2a7049b789db3537e1ac
Oscar19260 <oscar19260@gmail.com>
2024-04-11 18:01:26 -0600

    feat: update pros stuff

--- Pros_api/Robot15/include/main.h
+++ Pros_api/Robot15/include/main.h
@@ -1,81 +1,55 @@
-/**
- * \file main.h
- *
- * Contains common definitions and header files used throughout your PROS
- * project.
- *
- * \copyright Copyright (c) 2017-2023, Purdue University ACM SIGBots.
- * All rights reserved.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+/*----------------------------------------------------------------------------*/
+/*    Module:       main.h                                                    */
+/*    Author:       Vortex                                                    */
+/*    Created:      Wed Apr 19 2023                                           */
+/*    Description:  V5 project                                                */
+/*----------------------------------------------------------------------------*/
+
 
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
 
-/**
- * If defined, some commonly used enums will have preprocessor macros which give
- * a shorter, more convenient naming pattern. If this isn't desired, simply
- * comment the following line out.
- *
- * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
- * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
- * not convenient for most student programmers.
- */
 #define PROS_USE_SIMPLE_NAMES
-
-/**
- * If defined, C++ literals will be available for use. All literals are in the
- * pros::literals namespace.
- *
- * For instance, you can do `4_mtr = 50` to set motor 4's target velocity to 50
- */
 #define PROS_USE_LITERALS
 
 #include "api.h"
-
-/**
- * You should add more #includes here
- */
+#include "lemlib/api.hpp"
 //#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
-/**
- * If you find doing pros::Motor() to be tedious and you'd prefer just to do
- * Motor, you can use the namespace with the following commented out line.
- *
- * IMPORTANT: Only the okapi or pros namespace may be used, not both
- * concurrently! The okapi namespace will export all symbols inside the pros
- * namespace.
- */
-// using namespace pros;
-// using namespace pros::literals;
-// using namespace okapi;
-
-/**
- * Prototypes for the competition control tasks are redefined here to ensure
- * that they can be called from user code (i.e. calling autonomous from a
- * button press in opcontrol() for testing purposes).
- */
 #ifdef __cplusplus
 extern "C" {
-#endif
-void autonomous(void);
-void initialize(void);
-void disabled(void);
-void competition_initialize(void);
-void opcontrol(void);
-#ifdef __cplusplus
+    #endif
+    void autonomous(void);
+    void initialize(void);
+    void disabled(void);
+    void competition_initialize(void);
+    void opcontrol(void);
+    #ifdef __cplusplus
 }
 #endif
 
 #ifdef __cplusplus
-/**
- * You can add C++-only headers here
- */
-//#include <iostream>
-#endif
 
+//#include <iostream>
+
+//----------------- Robot constants -----------------//
+const double WHEEL_DIAMETER = 82.55;    //mm
+const double WHEEL_TRAVEL   = M_PI * WHEEL_DIAMETER;
+const double TRACK_WIDTH    = 230;      //mm
+const double TRACK_BASE     = 200;      //mm
+const double EXT_GEAR_RATIO = 1;    
+const double MOTOR_MAX_SPEED = 100;     //
+
+//----------------- Autonomous time -----------------//
+const double DIST_TO_BALL_1       = 30;
+const double TURN_TO_BALL_1       = 30;
+
+//------------------ User control -------------------//
+const double JOYSTICK_DEADBAND    = 10;
+
+#endif
 #endif  // _PROS_MAIN_H_
+
+#include <math.h>
+#include <corecrt_math_defines.h>
